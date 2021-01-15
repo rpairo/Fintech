8d809a014ee68c0170e0a0be60949be5e7a0b00a
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-01-15 13:18:32 +0100

    Initial Commit

---
+++ Fintech/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fintech
+//
+//  Created by Raúl Pera Pairó on 15/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fintech/FintechApp.swift
@@ -0,0 +1,17 @@
+//
+//  FintechApp.swift
+//  Fintech
+//
+//  Created by Raúl Pera Pairó on 15/1/21.
+//
+
+import SwiftUI
+
+@main
+struct FintechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
